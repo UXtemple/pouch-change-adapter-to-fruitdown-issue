cadb26e8531116b2fb63ccd1354f00ec707648f1
Darío Javier Cravero <dario@uxtemple.com>
2016-04-11 23:35:25 +0100

    initial commit

---
+++ test.js
@@ -0,0 +1,27 @@
+!function() {
+  'use strict';
+
+  function test() {
+    // open db with default adapter
+    var db = new PouchDB('test');
+
+    db.info().then(function(i1) {
+      console.log('default', i1);
+
+      // force fruitdown
+      db = new PouchDB('test', {adapter: 'fruitdown'});
+      db.info().then(function(i2) {
+        console.log('fruitdown', i2);
+      });
+    });
+  }
+
+  // make sure we idb is clear before running the test
+  window.indexedDB.webkitGetDatabaseNames().onsuccess = function(sender) {
+    for (var db in sender.target.result) {
+      indexedDB.deleteDatabase(sender.target.result[db]);
+    }
+
+    test();
+  }
+}();
